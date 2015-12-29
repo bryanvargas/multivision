da5cb1a12d0e5c478bb811f5d14e36d3a02646e2
Shingo Yabuki <bryanprsvar@gmail.com>
2015-12-29 19:38:23 -0300

    walking skeleton completado

---
+++ server.js
@@ -0,0 +1,75 @@
+var express = require('express'),
+    favicon =  require('serve-favicon'),
+    path = require('path'),
+    logger = require('morgan'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    mongoose = require('mongoose'),
+    stylus = require('stylus'),
+    nib = require('nib');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+function compile(str, path){
+    return stylus(str).set('filename', path);
+}
+
+
+app.set('views', __dirname + '/server/views');
+//app.set('view engine', 'jade');
+// view engine setup
+//app.use(express.logger('dev'));
+//app.use(express.bodyParser());
+app.set('views', path.join(__dirname, '/server/views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(stylus.middleware(
+    {
+        src: __dirname + '/public',
+        compile: compile
+    }
+));
+app.use(express.static(__dirname + '/public'));
+
+mongoose.connect('mongodb://localhost/multivision');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+    console.log('multivision db opened');
+});
+
+var messageSchema = mongoose.Schema( {message: String} );
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc){
+    mongoMessage = messageDoc.message;
+});
+
+
+
+app.get('/partials/:partialPath', function(req, res){
+    res.render('partials/' + req.params.partialPath);
+});
+
+app.get('*', function (req, res) {
+    res.render('index', {
+        mongoMessage: mongoMessage
+    });
+});
+
+
+
+var port = 3030;
+app.listen(port);
+
+console.log('Escuchando en el puerto ' + port + '...');
+
